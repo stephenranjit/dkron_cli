250a190bef3fc5f209e9102f4c5d41284efde1fb
Your Name <you@example.com>
2016-12-08 14:06:05 +0000

    adding cron format checker

--- dkron.rb
+++ dkron.rb
@@ -14,17 +14,17 @@ class Dkron < Thor
                 rest_cmd = "curl -m 2 -s -#{type} #{dkron_rest_url}"
                 puts "rest_cmd: #{rest_cmd}\n";
                 rest_cmd_response = `#{rest_cmd}`.chomp
-		if((rest_cmd_response == {}) || (rest_cmd_response == "") || (rest_cmd_response == "null"))
-			rest_cmd_response = nil
-		end
-	        puts "rest_cmd_response: #{rest_cmd_response}\n"
+        if((rest_cmd_response == {}) || (rest_cmd_response == "") || (rest_cmd_response == "null"))
+            rest_cmd_response = nil
+        end
+            puts "rest_cmd_response: #{rest_cmd_response}\n"
                 return rest_cmd_response
         end
 
         def GetInitHost
-		$dkron_servers = read_config()["dkron_servers"]
+        $dkron_servers = read_config()["dkron_servers"]
                 $dkron_servers.each { |server|
-			puts "checking #{server}...\n"
+            puts "checking #{server}...\n"
                         if(ExecRestQuery(server,"XGET","") != nil)
                                 $live_host = server
                                 break
@@ -37,28 +37,28 @@ class Dkron < Thor
                 return $live_host
         end
 
-	def read_config()
-		if(! File.file?($config_path))
-			puts "Could not find #{$config_path}. Please configure the tool before use\n"
-			exit
-		end
-		dkron_config_json = File.read($config_path)
+    def read_config()
+        if(! File.file?($config_path))
+            puts "Could not find #{$config_path}. Please configure the tool before use\n"
+            exit
+        end
+        dkron_config_json = File.read($config_path)
                 dkron_config_hash = JSON.parse(dkron_config_json)
-		return(dkron_config_hash)
-	end
-        
-	def toggle_state(job_name,state)
-		cron_details=getcron(job_name)
+        return(dkron_config_hash)
+    end
+
+    def toggle_state(job_name,state)
+        cron_details=getcron(job_name)
                 cron_details[:disabled]=state
                 init_host = GetInitHost()
                 puts ExecRestQuery(init_host,"XPOST","jobs -d '"+cron_details.to_json+"'")
-	end
-	
-	def check_cron_format(format)
-		if(format =~ /^\@/)
-			if(["yearly" "monthly" "weekly" "daily" "hourly" "minutely" "at" "every" "midnight" "annually"].include? format)
-				puts <<-LONGSTRING
-incorrect format: \@ can only follow one of these keywords:
+    end
+
+    def check_cron_format(format)
+        if(format =~ /^\@/)
+            if(format !~ /(yearly)|(monthly)|(weekly)|(daily)|(hourly)|(minutely)|(at)|(every)|(midnight)|(annually)/)
+                puts <<-LONGSTRING
+incorrect format: '\@' can only follow one of these keywords:
 yearly
 monthly
 weekly
@@ -70,25 +70,30 @@ every
 midnight
 annually
 LONGSTRING
-			end
-		elsif(format =~ /\d+\s+/)
-			if(format !~ /([\d\*]+\s?){6}/)
-				puts "incorrect format: please follow 6 space-separated fields\ne.g.: 0 * * * * *"
-			end
-		end
-	end
-	}
+                return("wrong")
+            end
+        elsif(format =~ /[\d\s]+/)
+            if(format !~ /(\S{1,2}\s){5}\S{1,2}/)
+                puts "incorrect format: please follow 6 space-separated fields\ne.g.: 0 * * * * *"
+                return("wrong")
+            end
+        else
+            puts "incorrect cron format\n"
+            return("wrong")
+        end
+    end
+    }
 
 
-	desc "configure","setup config file with variables"
-	option :servers, :required => true, :type => :array, :aliases => :s
-	def configure()
-		setup_hash = {:dkron_servers => options[:servers]}
-		File.open($config_path, 'w') { |config| config.write(setup_hash.to_json) }
-	end
-	
-        desc "remove", "Force leave a cron server"
-	option :server, :required => true, :aliases => :s
+    desc "configure","setup config file with variables"
+    option :servers, :required => true, :type => :array, :aliases => :s
+    def configure()
+        setup_hash = {:dkron_servers => options[:servers]}
+        File.open($config_path, 'w') { |config| config.write(setup_hash.to_json) }
+    end
+
+desc "remove", "Force leave a cron server"
+    option :server, :required => true, :aliases => :s
         def remove()
                 leader = getleader()
                 if(options[:server] != leader)
@@ -115,14 +120,17 @@ LONGSTRING
         \n
     > #{$0} setcron --job-name="get_hostname" --time="@at 2018-01-02T15:04:00Z" --command="/bin/hostname" --runon="rhel6" --notify=stephenranjit@co.com --parent=job1 --dependent=job2
         LONGDESC
-	option :job_name, :required => true, :aliases => :j
-	option :time, :required => true, :aliases => :t
-	option :command, :required => true, :aliases => :c
-	option :runon, :required => true, :aliases => :r
-	option :dependent, :default => nil, :type => :array, :aliases => :d
-	option :parent, :aliases => :p
-	option :notify, :aliases => :n
+    option :job_name, :required => true, :aliases => :j
+    option :time, :required => true, :aliases => :t
+    option :command, :required => true, :aliases => :c
+    option :runon, :required => true, :aliases => :r
+    option :dependent, :default => nil, :type => :array, :aliases => :d
+    option :parent, :aliases => :p
+    option :notify, :aliases => :n
         def setcron()
+                if(check_cron_format(options[:time]) == "wrong")
+                    exit
+                end
                 job_hash = {
                         :name => options[:job_name],
                         :command => options[:command],
@@ -141,39 +149,39 @@ LONGSTRING
                 puts ExecRestQuery(init_host,"XPOST","jobs -d '"+job_hash.to_json+"'")
         end
 
-	desc "disablecron","disable an existing cron job"
+    desc "disablecron","disable an existing cron job"
         option :job_name, :required => true, :aliases => :j
         def disablecron()
-        	toggle_state(options[:job_name],"true")
-	end
+            toggle_state(options[:job_name],"true")
+    end
 
-	desc "enaablecron","enable an existing cron job"
+    desc "enaablecron","enable an existing cron job"
         option :job_name, :required => true, :aliases => :j
         def enaablecron()
                 toggle_state(options[:job_name],"false")
         end
 
         desc "delcron","delete an existing cron job"
-	option :job_name, :type => :array, :required => true, :aliases => :j
+    option :job_name, :type => :array, :required => true, :aliases => :j
         def delcron()
                 init_host = GetInitHost()
-		options[:job_name].each { |job_name|
-	                puts ExecRestQuery(init_host,"XDELETE","jobs/#{job_name}")
-		}
+        options[:job_name].each { |job_name|
+                    puts ExecRestQuery(init_host,"XDELETE","jobs/#{job_name}")
+        }
         end
 
-        desc "getcron","get all or specified cron jobs"
-	option :job_name, :default => nil, :aliases => :j
+desc "getcron","get all or specified cron jobs"
+    option :job_name, :default => nil, :aliases => :j
         def getcron(job_name=nil)
                 init_host = GetInitHost()
-		if(job_name != nil)
-			job_name="jobs/#{job_name}"
-			puts "job_name = #{job_name}\n"
+        if(job_name != nil)
+            job_name="jobs/#{job_name}"
+            puts "job_name = #{job_name}\n"
                 elsif(options[:job_name] != nil)
-			job_name="jobs/#{options[:job_name]}"
-		else
-			job_name="jobs"
-		end 
+            job_name="jobs/#{options[:job_name]}"
+        else
+            job_name="jobs"
+        end
                 response = ExecRestQuery(init_host,"XGET","#{job_name}")
                 if(response != nil)
                         jobs_array = JSON.parse(response)
@@ -185,22 +193,22 @@ LONGSTRING
                 return jobs_array
         end
 
-	desc "getresult","get executions"
-	option :job_name, :required => true, :aliases => :j
-	def getresult
-		init_host = GetInitHost()
-		response = ExecRestQuery(init_host,"XGET","executions/#{options[:job_name]}")
-		if(response != nil)
+    desc "getresult","get executions"
+    option :job_name, :required => true, :aliases => :j
+    def getresult
+        init_host = GetInitHost()
+        response = ExecRestQuery(init_host,"XGET","executions/#{options[:job_name]}")
+        if(response != nil)
                         results_array = JSON.parse(response)
                 else
                         results_array = nil
                 end
                 pp results_array
 
-	end
+    end
 
         desc "runcron","on demand build"
-	option :job_name, :required => true, :aliases => :j
+    option :job_name, :required => true, :aliases => :j
         def runcron()
                 init_host = GetInitHost()
                 response = ExecRestQuery(init_host,"XPOST","jobs/#{options[:job_name]}")
